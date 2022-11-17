3c67694fc1ea575cbce9317787b08f5c43188f00
Fezz_Y <107613502+ItzJustDarkz99@users.noreply.github.com>
2022-11-17 14:58:13 +0530

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,25 @@
+var img="";
+var status="";
+function setup(){
+    canvas= createCanvas(640, 480);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd', modelloaded);
+    document.getElementById("status").innerHTML="Status : Dectecting Object...";
+}
+function modelloaded(){
+    console.log("Model Loaded");
+    status=true;
+    objectDetector.detect(img, gotresult);
+}
+function gotresult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+}
+function preload(){
+    img=loadImage("bedroom.jpg");
+}
+function draw(){
+    image(img, 0, 0, 640, 480);
+}
---
+++ main.js
@@ -0,0 +1,15 @@
+function bedroom(){
+    window.location="bedroom.html";
+}
+function bottles(){
+    window.location="bottle.html";
+}
+function desk(){
+    window.location="desk.html";
+}
+function fruitbasket(){
+    window.location="fruitbasket.html";
+}
+function tvandac(){
+    window.location="tv&ac.html";
+}
